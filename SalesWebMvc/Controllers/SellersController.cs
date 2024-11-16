43bbfdaf3021fbd4401aafb2c5a0581cdabc9e59
Ciceroferr <celsoa1978@gmail.com>
2024-11-16 13:01:56 -0300

    return custom error page

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -5,6 +5,7 @@ using SalesWebMvc.Services;
 using SalesWebMvc.Services.Exceptions;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -50,13 +51,13 @@ namespace SalesWebMvc.Controllers
         {
             if (id == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { Messafe = "Id not provided" });
             }
 
             var obj = _sellerService.FindById(id.Value);
             if(obj == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { Messafe = "Id not found" });
             }
 
             return View(obj);
@@ -75,27 +76,29 @@ namespace SalesWebMvc.Controllers
         {
             if (id == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { Messafe = "Id not provided" });
             }
             var obj = _sellerService.FindById(id.Value);
             if(obj == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { Messafe = "Id not found" });
             }
 
             return View(obj);
         }
 
+
+        [HttpGet]
         public IActionResult Edit(int? id)
         {
             if(id == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { Messafe = "Id not provided" });
             }
             var obj = _sellerService.FindById(id.Value);
             if(obj == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { Messafe = "Id not found" });
             }
 
             List<Department> departments = _departmentService.FindAll();
@@ -109,22 +112,28 @@ namespace SalesWebMvc.Controllers
         {
             if(id != seller.Id)
             {
-                return BadRequest();
+                return RedirectToAction(nameof(Error), new { Messafe = "Id mismatch" });
             }
             try
             {
                 _sellerService.Update(seller);
                 return RedirectToAction(nameof(Index));
             }
-            catch (NotFoundException)
+            catch (ApplicationException e)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { Messafe = e.Message });
             }
-            catch (DbConcurrencyException)
-            {
-                return NotFound();
-            }
+            
         }
         
+        public IActionResult Error(string message)
+        {
+            var viewModel = new ErrorViewModel
+            {
+                Message = message,
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier // pega o Id interno da requisição
+            };
+            return View(viewModel);
+        }
     }
 }
