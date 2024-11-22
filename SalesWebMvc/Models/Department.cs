17a31ee812536ade7a37ef4ea364b6a8aeab0a99
Ciceroferr <celsoa1978@gmail.com>
2024-11-22 08:24:24 -0300

    Implementig simple search

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -9,7 +9,7 @@ namespace SalesWebMvc.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public ICollection<Seller> Sellers { get; set; } = new List<Seller>(); //Ja esta instacinado a coleção 
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>(); //Esta instanciando a coleção 
 
         public Department()
         {
